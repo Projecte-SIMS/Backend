3ec0e5750f9849d3f89884d92f0d46ad027d3694
Jordimlies <jordimartinez@iesmontsia.org>
2026-01-28 17:09:33 +0100

    tickets

--- routes/api.php
+++ routes/api.php
@@ -4,10 +4,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\UserController;
 use App\Http\Controllers\Api\RoleController;
 use App\Http\Controllers\Api\AuthController;
-use App\Http\Controllers\Api\UserController;
-use App\Http\Controllers\Api\RoleController;
-use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\VehicleController;
+use App\Http\Controllers\TicketController;
+use App\Http\Controllers\TicketMessageController;
 
 // Rutas públicas de autenticación
 Route::post('/login', [AuthController::class, 'login']);
@@ -20,6 +19,7 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::apiResource('users', UserController::class);
     Route::apiResource('roles', RoleController::class);
     Route::apiResource('vehicles', VehicleController::class);
-    Route::apiResource('tickets', App\Http\Controllers\Api\TicketController::class);
-    Route::post('tickets/{ticket}/messages', [App\Http\Controllers\Api\TicketController::class, 'storeMessage']);
+    Route::apiResource('tickets', TicketController::class);
+    Route::post('tickets/{ticket}/messages', [TicketMessageController::class, 'store']);
+    Route::delete('tickets/{ticket}/messages/{message}', [TicketMessageController::class, 'destroy']);
 });
