c8e88be03a2a38226801cd46609275d3b693f93f
Jordimlies <jordimartinez@iesmontsia.org>
2026-04-14 15:48:37 +0200

    upd: landing

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    $landingPath = resource_path('views/welcome.blade.php');
-
-    if (is_file($landingPath)) {
-        return response()->file($landingPath, ['Content-Type' => 'text/html; charset=UTF-8']);
-    }
-
-    return response('<h1>Fleetly backend</h1><p>Landing no disponible.</p>', 200, [
-        'Content-Type' => 'text/html; charset=UTF-8',
-    ]);
+    return view('welcome');
 });
